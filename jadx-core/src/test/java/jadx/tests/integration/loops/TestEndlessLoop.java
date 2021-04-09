9d17d5ce7f844b558ee69f2ff4b8ba6a09ec4da6
RochelleAllan <rochelleaallan22@gmail.com>
2021-04-09 18:27:44 -0400

    third commit

--- jadx-core/src/test/java/jadx/tests/integration/loops/TestEndlessLoop.java
+++ jadx-core/src/test/java/jadx/tests/integration/loops/TestEndlessLoop.java
@@ -11,7 +11,8 @@ import static org.hamcrest.Matchers.containsString;
 public class TestEndlessLoop extends IntegrationTest {
 
 	public static class TestCls {
-
+		//TODO: test1() has a uesless comparison with itseld
+		//FIXME: Remove redundant test1()
 		void test1() {
 			while (this == this) {
 			}
